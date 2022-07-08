3731683159618dbf81f1cdbebf660452e06fe055
dima2959 <dima2959@gmnail.com>
2022-07-08 09:05:06 +0300

    try2

--- src/main/java/org/dima2959/CRUD/Spring/Boot331/dao/UserDaoImpl.java
+++ src/main/java/org/dima2959/CRUD/Spring/Boot331/dao/UserDaoImpl.java
@@ -16,34 +16,29 @@ public class UserDaoImpl implements UserDao {
     EntityManager entityManager;
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
         TypedQuery<User> query = entityManager.createQuery("select u from User u", User.class);
         return query.getResultList();
     }
 
     @Override
-    @Transactional
     public void addUser(User user) {
         entityManager.persist(user);
         entityManager.flush();
     }
 
     @Override
-    @Transactional
     public User readUser(long id) {
         return entityManager.find(User.class, id);
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         entityManager.merge(user);
         entityManager.flush();
     }
 
     @Override
-    @Transactional
     public void removeUser(long id) {
         User user = readUser(id);
         entityManager.remove(user);
--- src/main/java/org/dima2959/CRUD/Spring/Boot331/service/UserServiceImpl.java
+++ src/main/java/org/dima2959/CRUD/Spring/Boot331/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.dima2959.CRUD.Spring.Boot331.service;
 import org.dima2959.CRUD.Spring.Boot331.dao.UserDao;
 import org.dima2959.CRUD.Spring.Boot331.model.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,21 +17,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
+
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
     @Override
+    @Transactional
     public void removeUser(long id) {
         userDao.removeUser(id);
     }
